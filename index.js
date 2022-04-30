f31b1b67785d33753e5be997a24829fc9453fc11
Nicole Candiotti <nccandiotti@gmail.com>
2022-04-30 19:58:32 -0400

    first project (drum) complete

--- index.js
+++ index.js
@@ -1,2 +1,23 @@
 "strict"
-console.log("hi")
+
+document.addEventListener("keydown", handleKeydown)
+
+// handles key down  audio--------------------
+function handleKeydown(e) {
+  const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`)
+  const key = document.querySelector(`.key[data-key="${e.keyCode}"]`)
+  if (!audio) return
+  audio.currentTime = 0 //rewinds to start
+  audio.play()
+  key.classList.add("playing")
+}
+
+// handles keydown color transition
+const keys = document.querySelectorAll(".key")
+
+keys.forEach((key) => key.addEventListener("transitionend", removeTransition))
+
+function removeTransition(e) {
+  if (e.propertyName !== "transform") return
+  this.classList.remove("playing")
+}
