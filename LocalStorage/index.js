c2ab1a172515924a857cef9875d126c3893f2df2
Nicole Candiotti <nccandiotti@gmail.com>
2022-05-18 22:41:46 -0400

    2

--- LocalStorage/index.js
+++ LocalStorage/index.js
@@ -1,27 +1,11 @@
-// form element
 const addItems = document.querySelector(".add-items")
 const itemsList = document.querySelector(".plates")
 
 const items = JSON.parse(localStorage.getItem("items")) || []
-
-function populateList(plates = [], platesList) {
-  platesList.innerHTML = plates
-    .map((plate, i) => {
-      return `
-        <li>
-        <input type="checkbox" data-index=${i} id="items${i}" ${
-        plate.done ? "checked" : ""
-      }>
-          <label for ">${plate.text}</label>
-        </li>
-        `
-    })
-    .join("")
-  // .join at end turns array that the map made into one big string for the innerHTML
-}
-
+console.log(items)
 // on submit
 addItems.addEventListener("submit", handleSubmit)
+
 function handleSubmit(e) {
   e.preventDefault()
   const text = this.querySelector("[name=item]").value
@@ -37,4 +21,20 @@ function handleSubmit(e) {
 
   //   this refers to the form element that we are resetting
   this.reset()
+}
+
+function populateList(plates = [], platesList) {
+  platesList.innerHTML = plates
+    .map((plate, i) => {
+      return `
+        <li>
+        <input type="checkbox" data-index=${i} id="items${i}" ${
+        plate.done ? "checked" : ""
+      }>
+          <label for ">${plate.text}</label>
+        </li>
+        `
+    })
+    .join("")
+  // .join at end turns array that the map made into one big string for the innerHTML
 }
